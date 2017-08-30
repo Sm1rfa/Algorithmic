89526426f9ffe87b2eebe314e66b4234bcda78a3
Stoyan Bonchev <i401074@internal.financecorp.biz>
2017-08-30 14:47:11 +0200

    ad project

---
+++ Algorithmic/Algorithmic/Regular/FibonacciSeries.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithmic.Regular
+{
+    public class FibonacciSeries
+    {
+        // source https://www.dotnetperls.com/fibonacci
+        public int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
---
+++ Algorithmic/Algorithmic/Regular/Palindrome.cs
@@ -0,0 +1,27 @@
+﻿namespace Algorithmic.Regular
+{
+    public class Palindrome
+    {
+        // source https://www.dotnetperls.com/palindrome
+        public static bool IsPalindrome(string value)
+        {
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+    }
+}
