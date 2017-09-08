9f1a40ae4d50316e77b2132c5c38b5e082d7139e
Stoyan Bonchev <sm1rfs88@gmail.com>
2017-09-08 12:03:00 +0200

    add new common solutions for problems

---
+++ Algorithmic/Algorithmic/Regular/ArrayOutput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Algorithmic.Regular
+{
+    public class ArrayOutput
+    {
+        // method call
+        //static void Main(string[] args)
+        //{
+        //    int[] arr = new int[] { 1, 2, 3, 4, 5 };
+        //    fun1(ref arr);
+        //    Console.ReadLine();
+        //}
+
+        public void Fun1(ref int[] array)
+        {
+            for (int i = 0; i < array.Length; i = i + 2)
+            {
+                array[i] = array[i] + 10;
+            }
+            Console.WriteLine(string.Join(",", array));
+        }
+    }
+}
---
+++ Algorithmic/Algorithmic/Regular/CalculationOutput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Algorithmic.Regular
+{
+    public class CalculationOutput
+    {
+        public void Calculation1()
+        {
+            int x = 4, b = 2;
+            x -= b /= x * b;
+            Console.WriteLine(x + " " + b);
+            Console.ReadLine();
+        }
+
+        public void Calculation2()
+        {
+            int x = 8;
+            int b = 16;
+            int c = 64;
+            x /= c /= b;
+            Console.WriteLine(x + " " + b + " " + c);
+            Console.ReadLine();
+        }
+
+        public void Calculation3(int ii, double kk)
+        {
+            double j = ii + kk;
+            Console.WriteLine(j);
+        }
+
+        public void Calculation4(int ii, double kk)
+        {
+            double j = ii - kk;
+            Console.WriteLine(j);
+        }
+    }
+}
